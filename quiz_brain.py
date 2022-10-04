89b3dfc555709015897256b80efcd916a11c318f
Pawel Szabat <szabatpawel19@gmail.com>
2022-10-04 13:23:17 +0200

    Second version

--- quiz_brain.py
+++ quiz_brain.py
@@ -9,11 +9,11 @@ class QuizBrain:
     def next_question(self):
         current_question=self.question_list[self.question_number]
         self.question_number += 1
-        user_answer=input(f'Q.{self.question_number}: {current_question.text}? (True/False): ')
+        user_answer=input(f'Q.{self.question_number}: {current_question.text}? (True(t)/False(f)): ')
         self.check_answer(user_answer,current_question.answer)
 
     def check_answer(self, user_answer, corrrect_answer):
-        if user_answer.lower()==corrrect_answer.lower():
+        if user_answer.lower()==corrrect_answer.lower() or user_answer.lower()==corrrect_answer[0].lower():
             self.score+=1
             print('You got it right!')
         else:
